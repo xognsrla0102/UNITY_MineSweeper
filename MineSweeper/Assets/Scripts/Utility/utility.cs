e423327236632513868a312bbcbdb2d6b4e45023
김태훈 <xognsrla0102@gmail.com>
2021-12-20 19:10:42 +0900

    유틸리티 기능 추가
    - 유틸리티 폴더 생성
    - utility.cs 생성

---
+++ MineSweeper/Assets/Scripts/Utility/utility.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+public static class Utility
+{
+    public const int BLOCK_DIR = 8;
+    public static int[] dy = { -1, -1, -1, 0, 0, 1, 1, 1 };
+    public static int[] dx = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+    public const int SIZEY = 15;
+    public const int SIZEX = 15;
+
+    public static Color[] BLOCK_COLORS = new Color[(int)BlockType.COUNT]
+    {
+        new Color32(0xA4, 0xA4, 0xA4, 0xFF), // UNBROKEN
+        Color.white, // BROKEN
+        new Color32(0xFF, 0xF5, 0x00, 0xFF), // FLAG
+        new Color32(0x7D, 0xFF, 0x00, 0xFF), // QUESTION
+        new Color32(0xFF, 0x33, 0x33, 0xFF) // BOMB
+    };
+}
