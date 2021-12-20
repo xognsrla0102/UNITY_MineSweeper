d1ddbee7fb86c73ed7b4d4e378b76440536d1273
김태훈 <xognsrla0102@gmail.com>
2021-12-21 05:26:27 +0900

    사운드 추가
    - sfx, bgm 폴더 추가
    - 사운드 매니저 리팩토링

---
+++ MineSweeper/Assets/Scripts/Audio.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Audio
+{
+    [HideInInspector] public AudioSource source;
+
+    public BGM_Type bgmType;
+    public SFX_Type sfxType;
+
+    public AudioClip clip;
+    [Range(0, 1)] public float volume;
+    public bool isLoop;
+}
--- MineSweeper/Assets/Scripts/Manager/SoundManager.cs
+++ MineSweeper/Assets/Scripts/Manager/SoundManager.cs
@@ -1,39 +1,62 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public enum BGM_Type
 {
+    NONE,
     TITLE,
     IN_GAME,
+    GAME_CLEAR,
+    GAME_FAIL,
     BGM_NUM
 }
 
 public enum SFX_Type
 {
+    NONE,
     ON_CLICK,
-    BREAK_BLOCK,
+    BREAK_BLOCK_1,
+    BREAK_BLOCK_2,
+    BREAK_BLOCK_3,
+    BREAK_BLOCK_4,
     BOMB,
-    GAME_CLEAR
 }
 
 public class SoundManager : MonoBehaviour
 {
     public static SoundManager Instance;
 
-    [SerializeField] private AudioSource bgmSource;
-    [SerializeField] AudioClip[] bmpClips;
-
-    [SerializeField] private AudioSource sfxSource;
-    [SerializeField] AudioClip[] sfxClips;
+    public List<Audio> audios;
 
     public void Awake()
     {
         Instance = this;
+
+        foreach (var audio in audios)
+        {
+            audio.source = gameObject.AddComponent<AudioSource>();
+            audio.source.clip = audio.clip;
+
+            audio.source.volume = audio.volume;
+            audio.source.loop = audio.isLoop;
+        }
     }
 
     public void PlayBGM(BGM_Type bgmType)
     {
-        bgmSource.clip = bmpClips[(int)bgmType];
-        bgmSource.loop = bgmType != BGM_Type.IN_GAME;
-        bgmSource.Play();
+        Audio bgm = audios.Find(audio => audio.source.isPlaying);
+        if (bgm != null) bgm.source.Stop();
+
+        bgm = audios.Find(audio => audio.bgmType == bgmType);
+        if (bgm == null) return;
+
+        bgm.source.Play();
+    }
+
+    public void PlaySFX(SFX_Type sfxType)
+    {
+        Audio sfx = audios.Find(audio => audio.sfxType == sfxType);
+        if (sfx == null) return;
+        sfx.source.Play();
     }
 }
